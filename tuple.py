066c82baa84859ade256c127acf7a48d8006be75
naanko <nbarko04@gmail.com>
2022-09-12 14:49:05 +0100

    first commit

---
+++ tuple.py
@@ -0,0 +1,4 @@
+tuple1 =("Nanribet", "Nanko", "Miracle", "Lucky", "Wisdom", "Ramnan", "Dedan", "Pam", "Ponfa", "Yenbyen")
+tuple2 =(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+tuple3 = tuple1 + tuple2
+print(tuple3)
